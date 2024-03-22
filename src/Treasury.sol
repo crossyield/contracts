afdec66b6635c43824d801cd87b66687031202d8
0xYudhishthra <yudhishthra.m@gmail.com>
2024-03-23 01:01:42 +0800

    feat: init contract to create synthetic token for protocol's future yield generated

--- src/Treasury.sol
+++ src/Treasury.sol
@@ -1,6 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+//========================================================================
+//IMPORTS
+//========================================================================
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
