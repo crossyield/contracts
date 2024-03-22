d67d5fcc654f5817e2f82d2eecb7aa402399ba67
0xYudhishthra <yudhishthra.m@gmail.com>
2024-03-23 00:45:28 +0800

    feat: new events + errors declarations

--- src/Treasury.sol
+++ src/Treasury.sol
@@ -1,6 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-//import openzeppelin contracts
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+@title Treasury
+@notice This contract is used to store the funds of the protocol
+*/
+contract Treasury is Ownable {
+    //========================================================================
+    //ERRORS
+    //========================================================================
+    error InsufficientBalance(uint requested, uint available);
+    error InvalidAddress();
+    error TransferFailed();
+
+    //========================================================================
+    //EVENTS
+    //========================================================================
+    /// @dev Event for Ethers received.
+    event Received(address indexed sender, uint amount);
+
+    /// @dev Event for Ethers withdrawn.
+    event Withdrawn(address indexed beneficiary, uint amount);
+}
