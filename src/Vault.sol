123a0120df96338ce423127c3d4ad199e0a7fdb7
0xYudhishthra <yudhishthra.m@gmail.com>
2024-03-23 02:34:59 +0800

    feat: init vault contract for vault strategies tied to Dyson Finance

---
+++ src/Vault.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+//=============================================================================
+//IMPORTS
+//=============================================================================
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "./lib/solmate/src/tokens/ERC4626.sol";
+// import "./interfaces/ISyntheticToken.sol";
