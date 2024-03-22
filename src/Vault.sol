93e95e137414ed57e2534e6b143d63d8efe9bb95
0xYudhishthra <yudhishthra.m@gmail.com>
2024-03-23 02:50:08 +0800

    feat: init constructor and some state variables for vaults to track user data

--- src/Vault.sol
+++ src/Vault.sol
@@ -5,5 +5,24 @@ pragma solidity 0.8.24;
 //IMPORTS
 //=============================================================================
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "./lib/solmate/src/tokens/ERC4626.sol";
+import "../lib/solmate/src/tokens/ERC4626.sol";
 // import "./interfaces/ISyntheticToken.sol";
+
+/**
+@title Vault
+@notice This contract is used to store the yield strategies of the protocol
+*/
+contract Vault is ERC4626, ReentrancyGuard {
+    //=============================================================================
+    //STATE VARIABLES
+    //=============================================================================
+    struct VaultUser {
+        uint points;
+        uint debt;
+        uint credit;
+        uint collateral;
+        uint lastRedistributed;
+    }
+    mapping(address => VaultUser) public vaultUsers;
+    address[] public users;
+}
