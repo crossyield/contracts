f6d9542520224310a9b1e923577eb75a3efefef6
0xYudhishthra <yudhishthra.m@gmail.com>
2024-03-23 04:22:25 +0800

    feat: init interface for synthetic token contract

---
+++ src/interface/ISyntheticToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+interface ISyntheticToken {
+    // Error declarations can be included if they will be used externally.
+    error VaultNotWhitelisted();
+
+    // Function signatures
+    function mint(address _to, uint256 _amount) external;
+    function burn(address _from, uint256 _amount) external;
+    function addVault(address _vault) external;
+    function removeVault(address _vault) external;
+
+    // Admin role identifier function for external access
+    function ADMIN_ROLE() external view returns (bytes32);
+}
