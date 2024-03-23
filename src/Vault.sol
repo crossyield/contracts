dc4647798124dd83c3df864a2ee561d76c8e0f1f
0xYudhishthra <yudhishthra.m@gmail.com>
2024-03-23 22:06:41 +0800

    fix: math lib error

--- src/Vault.sol
+++ src/Vault.sol
@@ -386,12 +386,12 @@ contract Vault is ERC4626, ReentrancyGuard {
 
             //provide a fixed return for now
             token0Amt = 100_000_000;
-            USDC.safeTransferFrom(msg.sender, address(this), token0Amt);
+            USDC.safeTransferFrom(msg.sender, address(this), token0Amt * 1e12);
         } else {
             token0Amt = 0;
 
             token1Amt = 100_000_000;
-            USDC.safeTransferFrom(msg.sender, address(this), token1Amt);
+            USDC.safeTransferFrom(msg.sender, address(this), token1Amt * 1e12);
             // uint64 feeRatioAdded = uint64(
             //     (token1Amt * MAX_FEE_RATIO) / reserve1
             // );
